Finish fixing compute_loss function. Can now perform any combination of adversarial training, spectra regularization and jacobian regularization

train_code/utils.py
@@ -116,7 +116,8 @@ def generate_adv_images(model, images, targets, lossFunc, eps=50, cuda=False, ra
     return advImages
 
 
-def compute_loss(model, imgs, labels, loss, alpha_eig=0, alpha_adv=False, alpha_jacob=0, cuda=False, adversary=None):
+# TODO: USe faster jacobian regularization from Hoffman et al
+def compute_loss(model, imgs, labels, loss, alpha_spectra=0, alpha_adv=False, alpha_jacob=0, cuda=False, adversary=None):
     """
     Function that will compute the loss function PLUS any combination of the following three regularizers:
     1) Eigenvalue regularization, 2) Adversarial training, 3) Jacobian regularization
@@ -124,7 +125,7 @@ def compute_loss(model, imgs, labels, loss, alpha_eig=0, alpha_adv=False, alpha_
     :param imgs: batch of images
     :param labels: corresponding image labels
     :param loss: loss function being used
-    :param alpha_eig: weight of the regularizer term on the spectra. If it is 0, don't compute
+    :param alpha_spectra: weight of the regularizer term on the spectra. If it is 0, don't compute
     :param alpha_adv: boolean flag that will determine whether we should create a batch of images
     :param alpha_jacob: weight of the regularizer term for the jacobian. If it is 0, don't compute.
     :param cuda: Boolean flag that will determine whether we should use the GPU or not. It is assumed that the model is
@@ -132,22 +133,34 @@ def compute_loss(model, imgs, labels, loss, alpha_eig=0, alpha_adv=False, alpha_
     :param adversary: An object that is in charge of computing the adversarial examples
     :return: the value of the loss function and the value of the spectra and jacobian regularizer
     """
-    device = 'cuda' if cuda  else 'cpu'
+    device = 'cuda' if cuda else 'cpu'
+    if alpha_adv:
+        "If true, create adversarial images from batch"
+        adv_imgs = adversary.generate_adv_images(model.to(device), imgs.to(device), labels.to(device), loss.to(device))
+        imgs = torch.cat((imgs, adv_imgs), 0)  # concatenate images together
+
     "Compute a forward pass through the network and compute the loss"
     hidden, outputs = model.bothOutputs(imgs.to(device))  # feed data forward
     loss = loss(outputs, labels.to(device))  # compute loss
 
     "Compute regularizer"
-    regul = torch.zeros(1, device=device)
-    spectraTemp = []
-    if alpha > 0:
+    spectra_regul = torch.zeros(1, device=device)
+    spectra_temp = []
+    if alpha_spectra > 0:
         for idx in range(len(hidden)):
             spectra, rTemp = eigen_val_regulate(hidden[idx],
                                                 model.eigVec[idx], cuda=cuda)  # compute spectra for each hidden layer
             with torch.no_grad():
-                spectraTemp.append(spectra)
-            regul += rTemp
-    return loss, regul
+                spectra_temp.append(spectra)
+            spectra_regul += rTemp
+
+    jacob_regul = torch.zeros(1, device=device)
+    if alpha_jacob > 0:
+        jacobian = get_jacobian(model, imgs, labels, loss, cuda)  # get input-output jacobian
+        jacob_regul = torch.mean(jacobian ** 2)  # take frobenius norm of input-output jacobian divided by size of jacobian
+
+    total_loss = loss + alpha_spectra * spectra_regul + alpha_jacob * jacob_regul
+    return total_loss, loss, spectra_regul, spectra, jacob_regul
 
 
 def split_mnist(pathLoad='../simple_regularization/data/mnist.npy', fracVal=0.2):