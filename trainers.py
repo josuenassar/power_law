942020c87db7c765593143cb9c9d8f7693faf330
Josue <josue.nassar@stonybrook.edu>
2020-01-02 22:26:01 -0500

    Rewrite eigenvalue regularization class to use functions from utils

--- trainers.py
+++ trainers.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from typing import Callable
 from torch.utils.data.dataloader import DataLoader
 from torch import save
-from utils import JacobianReg, counter
+from utils import JacobianReg, counter, compute_eig_vectors, eigen_val_regulate
 from BatchModfier import BatchModifier
 
 
@@ -163,7 +163,7 @@ class JacobianRegularization(Trainer):
 
 class EigenvalueRegularization(Trainer):
 
-    def __init__(self, *, decoratee, save_name=None, max_iter=100_000, optimizer='adam', lr=1e-3,
+    def __init__(self, *, decoratee: BatchModifier, save_name=None, max_iter=100_000, optimizer='adam', lr=1e-3,
                  weight_decay=1e-5, alpha_spectra):
 
         # import pdb; pdb.set_trace()
@@ -188,66 +188,17 @@ class EigenvalueRegularization(Trainer):
         spectra_regul = torch.zeros(1, device=self.device)
         spectra_temp = []
         for idx in range(len(hidden)):
-            spectra, rTemp = self.eigen_val_regulate(hidden[idx], self.eig_vec)  # compute spectra for each hidden layer
+            spectra, rTemp = eigen_val_regulate(hidden[idx], self.eig_vec, start=self.eig_start, device=self.device)
             with torch.no_grad():
                 spectra_temp.append(spectra)
             spectra_regul += rTemp
         return loss + self.alpha_spectra * spectra_regul
 
-    def eigen_val_regulate(self, x, v):
-        """
-        Function that approximates the eigenvalues of the matrix x, by finding them wrt some pseudo eigenvectors v and then
-        penalizes eigenvalues that stray too far away from a power law
-        :param x: hidden representations, N by D
-        :param v: eigenvectors, D by D (each column is an eigenvector!)
-        :param eigT: if the eigenspectra is already estimated, can just be passed in, else it is default as None
-        :param start: index that states what eigenvalues to start regulating.
-        :return: value of regularizer and the estimated spectra
-        """
-        eigT = self.eig_T
-        start = self.eig_start
-        if eigT is None:
-            xt = x - torch.mean(x, 0)  # demean the data
-            cov = xt.transpose(1, 0) @ xt / (x.shape[0] - 1)  # compute covariance matrix
-            cov = (cov + cov.transpose(1, 0)) / 2
-            eig = torch.diag(v.transpose(1, 0) @ cov @ v).to(self.device)
-
-        else:
-            eig = eigT
-
-        eigs = torch.sort(eig, descending=True)[0]
-        regul = torch.zeros(1, device=self.device)
-        # slope = -1
-        with torch.no_grad():
-            alpha = eigs[start] * (start + 1)  # let the the constant be the largest eigenvalue
-
-        for n in range(start + 1, eigs.shape[0]):
-            if eigs[n] > 0:  # don't use negative eigenvalues
-                regul += (eigs[n] / (alpha / (n + 1)) - 1) ** 2 + torch.relu(eigs[n] / (alpha / (n + 1)) - 1)
-        return eigs, regul / x.shape[0]
-
     def compute_eig_vectors(self, x, y):
-        hidden, outputs = self.bothOutputs(x.to(self.device))
-        loss = self.loss(outputs, y.to(self.device))
-
-        spectraTemp = []
-        eigVec = []
-        regul = torch.zeros(1, device=self.device)
-
-        for idx in range(len(hidden)):
-            hTemp = hidden[idx] - torch.mean(hidden[idx], 0)
-            cov = hTemp.transpose(1, 0) @ hTemp / hTemp.shape[0]  # compute covariance matrix
-            cov = cov.double()  # cast as 64bit to mitigate underflow in matrix factorization
-            cov = (cov + cov.transpose(1, 0)) / 2
-            _, eigTemp, vecTemp = torch.svd(cov, compute_uv=True)  # compute eigenvectors and values
-            self.eig_T = eigTemp.float()
-            vecTemp = vecTemp.float()
-            eigTemp, rT = self.eigen_val_regulate(0, 0)  # compute regularizer
-            regul += rT
-            spectraTemp.append(eigTemp.cpu())  # save spectra
-            self.eig_vec.append(vecTemp)
-
-        return eigVec, loss, spectraTemp, regul.cpu().item()
+        with torch.no_grad():
+            eigVec, loss, spectraTemp, regul = compute_eig_vectors(x, y, self._architecture, self.loss, self.device)
+        self.eig_vec = eigVec
+        return eigVec, loss, spectraTemp, regul
 
 
 class EigenvalueAndJacobianRegularization(Trainer):
