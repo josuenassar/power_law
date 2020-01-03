d54758fb62a34a491f3c12fc82fc3cef02c0e633
Josue <josue.nassar@stonybrook.edu>
2020-01-03 00:07:35 -0500

    misspelled tqdm lol

--- trainers.py
+++ trainers.py
@@ -33,7 +33,7 @@ class Trainer(nn.Module):
         return self._batch_modifier(x)
 
     def train_epoch(self, X: DataLoader):
-        for _, (x, y) in enumerate(tqmd(X)):
+        for _, (x, y) in enumerate(tqdm(X)):
             self.train_batch(x, y)
 
     @counter
