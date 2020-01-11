187360b043e579ec4c6b9482f187d09fa2e102b9
Josue <josue.nassar@stonybrook.edu>
2020-01-10 23:54:43 -0500

    Updated

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from tqdm import tqdm
 # In[]
 "Create model"
 kwargs = {"dims": [(28 * 28, 1000), (1000, 10)], "activation": "relu", "architecture": "mlp",
-          "trainer": "vanilla", "regularizer": "eigjac", 'alpha_spectra': 1e-3, 'alpha_jacob':1e-4,
+          "trainer": "adv", "regularizer": "eigjac", 'alpha_spectra': 1e-3, 'alpha_jacob':1e-4,
           'optimizer': 'adam', 'lr': 1e-3, 'weight_decay': 0, 'cuda': False, 'eps': 0.3,
-          'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'PGD'}
+          'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'FGSM'}
 model = ModelFactory(**kwargs)
 
 # In[]
