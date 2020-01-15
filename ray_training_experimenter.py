aaf4fe9bfc51149ad49e706e476fb01b731bef00
Josue <josue.nassar@stonybrook.edu>
2020-01-15 16:55:58 -0500

    Code for loading models on queue

--- ray_training_experimenter.py
+++ ray_training_experimenter.py
@@ -1,16 +1,10 @@
 import ray
-import ray.tune as tune
-from ray.tune.schedulers import FIFOScheduler
-from ray.tune.suggest.skopt import SkOptSearch
-from skopt.space import Real
-from skopt import Optimizer
 from multiprocessing import cpu_count
 from torch.cuda import device_count
 import argparse
 import uuid
 import os
 from sacred.observers import MongoObserver
-from pymongo import MongoClient, DESCENDING
 
 
 
@@ -19,103 +13,66 @@ nGPU = device_count()
 load = 1/2  # how large a fraction of the GPU memory does the model take up 0 <= load <=  1
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--lr', type=float, default=argparse.SUPPRESS)
-argparser.add_argument('--modelType',
-                       choices=['mlp', 'cnn'],
-                       default=argparse.SUPPRESS,
-                       help='Type of neural network.')
-argparser.add_argument('--activation',
-                       choices=['relu', 'tanh'],
+argparser.add_argument("--activation",  choices=['relu', 'tanh'],
                        help='Nonlinearity.',default=argparse.SUPPRESS)
-argparser.add_argument('--advTraining', action='store_true')
-argparser.add_argument('--pathSave', type=str, required=True)
-argparser.add_argument('--pathLoad', type=str, required=True)
-argparser.add_argument('--batch_size', type=int, default=argparse.SUPPRESS)
-argparser.add_argument('--alpha', type=float, default=argparse.SUPPRESS)
-argparser.add_argument('--gradSteps', type=int, default=argparse.SUPPRESS)
-argparser.add_argument('--noRestarts', type=int, default=argparse.SUPPRESS)
-argparser.add_argument('--numEpochs', default=100_000, type=int)
+argparser.add_argument("--alpha_jacob",  type=float, default=argparse.SUPPRESS)
+argparser.add_argument("--alpha_spectra",  type=float, default=argparse.SUPPRESS)
+argparser.add_argument("--architecture", choices=["LeNet5", "MadryMNIST", "mlp", "cnn"], required=True,
+                       help='Type of neural network.')
+argparser.add_argument("--cuda", default=True)
+argparser.add_argument("--data_dir", required=True)
+argparser.add_argument("--dataset", choices=["MNIST", "CIFAR10"], default=argparse.SUPPRESS)
+argparser.add_argument("--dims", type=str, default=argparse.SUPPRESS)
 argparser.add_argument('--eps', type=float, default=argparse.SUPPRESS)
-argparser.add_argument('--dims', type=str, default=argparse.SUPPRESS)
-argparser.add_argument('--smoke-test', action="store_true", default=False)
-argparser.add_argument('--host', choices=['seawulf', 'orion', 'dirac'], help='Server where the code ran',
-                       default='seawulf')
-argparser.add_argument('--reps', help='Server where the code ran', type=int,
-                       default=5)
-args, unknownargs = argparser.parse_known_args()
+argparser.add_argument('--gradSteps', type=int, default=argparse.SUPPRESS)
+argparser.add_argument("--lr", default=argparse.SUPPRESS)
+argparser.add_argument("--lr_pgd", default=argparse.SUPPRESS)
+argparser.add_argument("--max_epochs", type=int, default=500)
+argparser.add_argument("--max_iter", type=int, default=argparse.SUPPRESS)
+argparser.add_argument('--noRestarts', type=int, default=argparse.SUPPRESS)
+argparser.add_argument("--optimizer", choices=["adam", "rms", "sgd"], default=argparse.SUPPRESS)
+argparser.add_argument("--regularizer", choices=["no", "jac", "eig", "eigjac"], default="no")
+argparser.add_argument("--save_dir", required=True)
+argparser.add_argument("--trainer", choices= ["vanilla", "adv"], default="vanilla")
+argparser.add_argument("--training_type", choices=["FGSM", "PGD"], default=argparse.SUPPRESS)
+argparser.add_argument("--hpsearch", type=bool, default=False)
+argparser.add_argument("--weight_decay", type=float, default=0.)
 
+argparser.add_argument('--smoke-test', action="store_true", default=False)
+args, unknownargs = argparser.parse_known_args()
 args = vars(args)
 try :
     args['dims'] = eval(args['dims'])
 except:
     pass
-TT = args['advTraining']
+
 smoke_test = args['smoke_test']
-host = args['host']
-[args.pop(k) for k in ['advTraining','smoke_test','host']]
-args['pathSave'] = os.path.join(args['pathSave'], 'tmp' + str(uuid.uuid4())[:8])
-
-# #TODO query mongoDB and create config
-# client = MongoClient("mongodb://powerLawNN:Pareto_a^-b@ackermann.memming.com/admin?authMechanism=SCRAM-SHA-1")
-# db = client.powerLawHypers
-
-# if TT:
-#     config = db['runs'].find({
-#         "$and": [{"config.eps": args['eps']},
-#                  {"config.alpha": args['alpha']},
-#                  {"config.batch_size": args['batch_size']},
-#                  {"config.modelType": args['modelType']}
-#                  ]},
-#         {
-#             "config.lr": 1,
-#         }
-#     ).sort("result", DESCENDING).limit(1)[0]['config']
-# else:
-#     config = db['runs'].find({
-#         "$and": [{"config.alpha": args['alpha']},
-#                  {"config.batch_size": args['batch_size']},
-#                  {"config.modelType": args['modelType']}
-#                  ]},
-#         {
-#             "config.lr": 1,
-#         }
-#     ).sort("result", DESCENDING).limit(1)[0]['config']
+if args["architecture"] in ["LeNet5", "MadryMNIST"]:
+    architecture = args["architecture"]
+    [args.pop(k) for k in ['architecture']]
+    try:
+        args.pop("dims")
+    except:
+        pass
+args.pop('smoke_test')
 
 
 @ray.remote(num_gpus=load, num_cpus=int(load * nCPU/nGPU))
-def train():
+def train(config):
     import time, random
+    from experiment import ex  # importing experiment here is crucial!
     time.sleep(random.uniform(0.0, 10.0))
-    if TT:
-        from advTraining import ex
-    else:
-        from training import ex
     if smoke_test:
-        config = {'numEpochs': 1,**config}
+        config = {'numEpochs': 1, **config}
     mongoDBobserver = MongoObserver.create(
         url='mongodb://powerLawNN:Pareto_a^-b@ackermann.memming.com/admin?authMechanism=SCRAM-SHA-1',
         db_name='powerLawExpts')
     ex.observers.append(mongoDBobserver)
-    ex.run(config_updates={**args, **config})
+    ex.run(named_configs=[architecture], config_updates={**args, **config})
     result = ex.current_run.result
 
-jobid  = os.popen("sacct -n -X --format jobid --name twists77.job").read().strip().strip('\n')
+
+jobid = os.popen("sacct -n -X --format jobid --name twists77.job").read().strip().strip('\n')
 if __name__ == '__main__':
-    import tweepy
-    consumer_key = "emYE7xtZ3gdw48OuGyQ50xTXB"
-    consumer_secret = "lEQRqnXLkT3Hh2V8fZ5gCOoUslJeXj0wzWnTupkabEgNkIVCOg"
-    access_token = "1007403120899579904-veqrICIYFfSnfYtvWcb7oryiuWXc2h"
-    access_token_secret = "eimBG9y1nd6bawHZGmwQf2dkQRhRPWK4R1X6yQ6eq0lZq"
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    # DO TRAINING & SAVE TO DB
     ray.init(num_cpus=nCPU//2, num_gpus=1)
     [train.remote() for i in range(args['reps'])]
-
-    if TT:
-        api.update_status(status="Evaluated {} adversarially trained models for a power_law_nn {} with {} layer dims "
-                                 "on {}.".format(args['reps'], args['modelType'], str(args['dims']),host))
-    else:
-        api.update_status(status="Evaluated {} penalized models for a power_law_nn {} with {} layer dims "
-                                 "on {}.".format(args['reps'], args['modelType'], str(args['dims']), host))
