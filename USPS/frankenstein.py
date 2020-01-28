Was normalizing wrong by accident

USPS/frankenstein.py
@@ -15,12 +15,12 @@ with h5py.File('usps.h5', 'r') as hf:
         y_te = test.get('target')[:]
 
 # In[]
-temp = np.reshape(X_te / 256, (2007, 16, 16))  # Reshape and scale to appropriate range
+temp = np.reshape(X_te, (2007, 16, 16))  # Reshape and scale to appropriate range
 
 # In[]
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.imshow(temp[-1, :, :])
+ax.imshow(temp[10, :, :])
 ax.set_aspect('equal')
 fig.show()
 
@@ -29,10 +29,10 @@ new_temp = np.zeros((2007, 28, 28))
 for n in tqdm(range(2007)):
     new_temp[n, :, :] = imresize(temp[n, :, :], (28, 28))
 
-# In[]
+# In[]                  
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.imshow(new_temp[-1, :, :])
+ax.imshow(new_temp[10, :, :])
 ax.set_aspect('equal')
 fig.show()
 