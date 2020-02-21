ef6a3e7962579c25e9a7bc84a43e0ca228da7efd
Josue <josue.nassar@stonybrook.edu>
2020-02-21 14:23:24 -0500

    updated test script

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ from tqdm import tqdm
 import torch
 # In[]
 "Create model"
-# kwargs = {"dims": [(28 * 28, 1000), (1000, 10)], "activation": "relu", "architecture": "mlp",
+kwargs = {"dims": [(28 * 28, 1000), (1000, 1000), (1000, 1000), (1000, 10)], "activation": "relu", "architecture": "mlp",
+          "trainer": "vanilla", "regularizer": "eig", 'alpha_spectra': 1e-3, 'alpha_jacob':1e-4,
+          'optimizer': 'adam', 'lr': 1e-3, 'weight_decay': 0, 'cuda': False, 'eps': 0.3,
+          'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'FGSM', 'slope': [1.04, 1.02, 1.0]}
+# kwargs = {"dims":[2, (1, 32), (32, 64), (1024, 1024), (1024, 10)], "activation": "tanh", "architecture": "mlp",
 #           "trainer": "adv", "regularizer": "eigjac", 'alpha_spectra': 1e-3, 'alpha_jacob':1e-4,
 #           'optimizer': 'adam', 'lr': 1e-3, 'weight_decay': 0, 'cuda': False, 'eps': 0.3,
-#           'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'FGSM'}
-kwargs = {"dims":[2, (1, 32), (32, 64), (1024, 1024), (1024, 10)], "activation": "tanh", "architecture": "cnn",
-          "trainer": "adv", "regularizer": "eigjac", 'alpha_spectra': 1e-3, 'alpha_jacob':1e-4,
-          'optimizer': 'adam', 'lr': 1e-3, 'weight_decay': 0, 'cuda': True, 'eps': 0.3,
-          'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'PGD'}
+#           'gradSteps': 40, 'noRestarts': 1, 'lr_pgd': 1e-2,  'training_type': 'PGD', 'only_last': True}
 model = ModelFactory(**kwargs)
 
 # In[]
 "Get data loader"
-train_loader, test_loader, full_loader = get_data(dataset='MNIST', batch_size=5000, _seed=0,
+train_loader, test_loader, full_loader = get_data(dataset='MNIST', batch_size=100, _seed=0,
                                                   validate=False, data_dir='')
 X_full, Y_full = next(iter(full_loader))
 # In[]
