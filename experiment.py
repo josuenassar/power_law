db38d41cfc8846f1075a13e2d1cf7c2fcfe97e8c
Josue <josue.nassar@stonybrook.edu>
2020-02-22 14:37:16 -0500

    allow bn to be passed as a boolean variable

--- experiment.py
+++ experiment.py
@@ -57,6 +57,7 @@ def Shallow():
     """
     architecture = "mlp"
     dims = [(28 * 28, 2_000), (2_000, 10)]
+    bn = False
 
 
 @ex.named_config
@@ -95,13 +96,14 @@ def cfg():
     hpsearch = True
     weight_decay = 0
     only_last = False
+    bn = True
 
 
 @ex.automain
 def do_training(activation, alpha_jacob, alpha_spectra, architecture, cuda, data_dir,
                 dataset, dims, eps, gradSteps, lr, lr_pgd, max_epochs, max_iter, noRestarts,
                 optimizer, regularizer, save_dir, trainer, only_last,
-                training_type, hpsearch, weight_decay, _seed, _run):
+                training_type, hpsearch, weight_decay, bn, _seed, _run):
 
     os.makedirs(save_dir, exist_ok=True)
     torch.manual_seed(_seed)
@@ -111,7 +113,7 @@ def do_training(activation, alpha_jacob, alpha_spectra, architecture, cuda, data
                     'alpha_jacob': alpha_jacob, 'optimizer': optimizer, 'lr': lr, 'lr_pgd': lr_pgd,
                     'weight_decay': weight_decay, 'noRestarts':noRestarts,
                     'cuda': cuda, 'eps': eps, 'gradSteps': gradSteps,  'training_type': training_type,
-                    "max_iter": max_iter, 'only_last': only_last}
+                    "max_iter": max_iter, 'only_last': only_last, 'bn': bn}
 
     model = ModelFactory(**model_kwargs)
     # Store where model should be saved
