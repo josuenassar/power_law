58f384ff658c5bf769eb68c7d94d5315b845fce0
Josue <josue.nassar@stonybrook.edu>
2020-05-13 19:02:23 -0400

    i don't need 2 equal signs

--- DataDefs/data.py
+++ DataDefs/data.py
@@ -36,11 +36,11 @@ def get_data(dataset, batch_size, _seed, validate, data_dir):
         if dataset == "CIFAR10":
             train_set = datasets.CIFAR10(root=data_dir, train=True, download=True,  transform=transform)
         elif dataset == "CIFAR10Augmented":
-            augmented_transform = transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
-                                                                  transforms.RandomCrop(32, padding=4),
-                                                                  transforms.RandomHorizontalFlip(),
-                                                                  transforms.ToTensor(),
-                                                                  transforms.Normalize((0.4810,), (0.2392,))])
+            augmented_transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
+                                                      transforms.RandomCrop(32, padding=4),
+                                                      transforms.RandomHorizontalFlip(),
+                                                      transforms.ToTensor(),
+                                                      transforms.Normalize((0.4810,), (0.2392,))])
             train_set = datasets.CIFAR10(root=data_dir, train=True, download=True,  transform=augmented_transform)
 
         test_set = datasets.CIFAR10(root=data_dir, train=False, download=True,  transform=transform)
