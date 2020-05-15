2d1ba975f6e4b7cef2e9684120fd1ddb3b667879
Josue <josue.nassar@stonybrook.edu>
2020-05-15 12:04:46 -0400

    update

--- neurips_experiments/experiment1.py
+++ neurips_experiments/experiment1.py
@@ -29,8 +29,8 @@ def bad_boy_vibes(tau=0, activation='tanh', cuda=False, num_epochs=50, parallel=
     :param tau: integer, at what point in the eigenvalue spectrum should we regularize
     """
     realizations = 3
-    batch_size = 2500  # 1.25 times the widest layer in the network
-    lr = 1e-3
+    batch_size = 3500  # 1.25 times the widest layer in the network
+    lr = 1e-4
     train_loader, _, full_loader = get_data(dataset='MNIST', batch_size=batch_size, _seed=0,
                                             validate=False, data_dir='')
     if vanilla:
@@ -66,8 +66,8 @@ def bad_boy_vibes(tau=0, activation='tanh', cuda=False, num_epochs=50, parallel=
                    'experiment_1/vanilla' + '_activation=' + activation + '_epochs=' + str(num_epochs))
 
     else:
-        slopes = [1.06, 1.04, 1.02, 1.00]
-        regularizers_strengths = [0.1, 1., 5.]
+        slopes = [1.00]
+        regularizers_strengths = [1., 2., 5.]
         stuff_to_loop_over = product(slopes, regularizers_strengths)
         # In[]
         "Load in data loader"
