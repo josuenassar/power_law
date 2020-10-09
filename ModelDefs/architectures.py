8c672fb28e1509d28fa14b6e6018551f126cb71a
Josue <josue.nassar@stonybrook.edu>
2020-10-09 16:30:27 -0400

    no batchnorm in BasicBlock

--- ModelDefs/architectures.py
+++ ModelDefs/architectures.py
@@ -296,53 +296,84 @@ class CNN_Flat(CNN):
         return hidden, self.linSequential[-1](hidden[-1])
 
 
-# ResNet vibes
-class VGG_Block(nn.Module):
-    def __init__(self, in_channels, out_channels, activation='relu'):
-        super().__init__()
-        block = []
-        block.append(nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=1))
-        if activation == 'relu':
-            block.append(nn.ReLU())
-        else:
-            block.append(nn.Tanh())
+"ResNet vibes"
 
-        block.append(nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3)))
-        if activation == 'relu':
-            block.append(nn.ReLU())
-        else:
-            block.append(nn.Tanh())
-        block.append(nn.MaxPool2d(kernel_size=(2, 2)))
-        self.block = nn.Sequential(*block)
+
+def _weights_init(m):
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
 
     def forward(self, x):
-        return self.block(x)
+        return self.lambd(x)
 
 
-class VGG3(ModelArchitecture):
-    def __init__(self, *, dims, activation='relu', bn=False, cuda=False):
-        super().__init__(cuda=cuda)
-        self.block1 = VGG_Block(1, 16, activation=activation)
-        self.block2 = VGG_Block(16, 32, activation=activation)
-        # self.block3 = VGG_Block(32, 64, activation=activation)
-        self.block3 = nn.Linear(1152, 1152)
-        self.numHiddenLayers = 3
-        self.fc = nn.Linear(1152, 10)
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        # self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        # self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
 
     def forward(self, x):
-        out = self.block1(x)
-        out = self.block2(out)
-        # out = self.block3(out)
-        out = self.block3(out.view(out.size(0), -1))
-        return self.fc(out.view(out.size(0), -1))
+        out = F.relu(self.conv1(x))
+        out = self.conv2(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-    def bothOutputs(self, x):
-        hiddens = []
-        out = self.block1(x)
-        hiddens.append(out.view(out.size(0), -1))
-        out = self.block2(out)
-        hiddens.append(out.view(out.size(0), -1))
-        # out = self.block3(out)
-        out = self.block3(out.view(out.size(0), -1))
-        hiddens.append(out.view(out.size(0), -1))
-        return hiddens, self.fc(out.view(out.size(0), -1))
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
