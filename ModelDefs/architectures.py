7211ec9746162916ccb4607957a3b98d0cc272d3
Josue <josue.nassar@stonybrook.edu>
2020-10-13 15:45:30 -0400

    seems like we need batch norm

--- ModelDefs/architectures.py
+++ ModelDefs/architectures.py
@@ -321,9 +321,9 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1, option='A'):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
-        # self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=True)
-        # self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
@@ -340,8 +340,8 @@ class BasicBlock(nn.Module):
                 )
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = self.conv2(out)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -353,7 +353,7 @@ class ResNet(ModelArchitecture):
         self.in_planes = 8
 
         self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1, bias=True)
-        # self.bn1 = nn.BatchNorm2d(16)
+        self.bn1 = nn.BatchNorm2d(8)
         self.layer1 = self._make_layer(block, 8, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 16, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 32, num_blocks[2], stride=2)
@@ -371,7 +371,7 @@ class ResNet(ModelArchitecture):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
+        out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -383,7 +383,7 @@ class ResNet(ModelArchitecture):
     def bothOutputs(self, x):
         hiddens = []
         # First block
-        out = F.relu(self.conv1(x))
+        out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         hiddens.append(out.view(out.size(0), -1))
 
