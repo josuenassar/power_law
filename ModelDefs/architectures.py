f300de6a32042e68b172fecf9881d20e603375c8
Josue <josue.nassar@stonybrook.edu>
2020-10-15 01:06:37 -0400

    try this

--- ModelDefs/architectures.py
+++ ModelDefs/architectures.py
@@ -350,14 +350,14 @@ class BasicBlock(nn.Module):
 class ResNet(ModelArchitecture):
     def __init__(self, block, num_blocks, num_classes=10, cuda=False):
         super(ResNet, self).__init__(cuda=cuda)
-        self.in_planes = 8
+        self.in_planes = 4
 
-        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1, bias=True)
-        self.bn1 = nn.BatchNorm2d(8)
-        self.layer1 = self._make_layer(block, 8, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 16, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 32, num_blocks[2], stride=2)
-        self.linear = nn.Linear(32, num_classes)
+        self.conv1 = nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(4)
+        self.layer1 = self._make_layer(block, 4, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 8, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 16, num_blocks[2], stride=2)
+        self.linear = nn.Linear(16, num_classes)
 
         self.apply(_weights_init)
 
