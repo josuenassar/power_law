12f116bc55d8ab706ba6b3690d4d2f925fd29396
Josue <josue.nassar@stonybrook.edu>
2020-10-17 17:04:41 -0400

    add vg11

--- ModelDefs/architectures.py
+++ ModelDefs/architectures.py
@@ -298,8 +298,6 @@ class CNN_Flat(CNN):
 
 
 "ResNet vibes"
-
-
 def _weights_init(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
@@ -422,3 +420,85 @@ class ResNet(ModelArchitecture):
 
 def resnet20(cuda=False, n_filters=4, cp=True, nchunks=1):
     return ResNet(BasicBlock, [3, 3, 3], cuda=cuda, n_filters=n_filters, cp=cp, nchunks=nchunks)
+
+
+
+"VGG vibes"
+import math
+import torch.nn as nn
+import torch.nn.init as init
+
+
+class VGG(ModelArchitecture):
+    '''
+    VGG model
+    '''
+    def __init__(self, features, cuda=False):
+        super(VGG, self).__init__(cuda=cuda)
+        self.features = features
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(),
+        #     nn.Linear(512, 512),
+        #     nn.ReLU(True),
+        #     nn.Dropout(),
+        #     nn.Linear(512, 512),
+        #     nn.ReLU(True),
+        #     nn.Linear(512, 10),
+        # )
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 10),
+        )
+        self.numHiddenLayers = 1
+        # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def bothOutputs(self, x, only_last=False):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        hiddens = [x]
+        x = self.classifier(x)
+        return hiddens, x
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M',
+          512, 512, 512, 512, 'M'],
+}
+
+
+def vgg11(cuda=False):
+    """VGG 11-layer model (configuration "A")"""
+    return VGG(make_layers(cfg['A']), cuda=cuda)
--- ModelDefs/models.py
+++ ModelDefs/models.py
@@ -4,7 +4,7 @@ import unittest
 import os
 
 from torch.utils.data.dataloader import DataLoader
-from ModelDefs.architectures import MLP, CNN, Flat, CNN_Flat, resnet20
+from ModelDefs.architectures import MLP, CNN, Flat, CNN_Flat, resnet20, vgg11
 from ModelDefs.BatchModfier import AdversarialTraining, MLTraining
 from ModelDefs.trainers import NoRegularization, JacobianRegularization,\
     EigenvalueAndJacobianRegularization, EigenvalueRegularization
@@ -29,6 +29,7 @@ def ModelFactory(**kwargs):
                'flat': Flat,
                'cnn_flat': CNN_Flat,
                'resnet': resnet20,
+               'vgg': vgg11,
                'adv': AdversarialTraining,
                'vanilla': MLTraining,
                'no': NoRegularization,
