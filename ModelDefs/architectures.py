7613ee0c3db1e029e0678395fe361473ebf42035
Josue <josue.nassar@stonybrook.edu>
2020-10-17 17:31:36 -0400

    go back to og

--- ModelDefs/architectures.py
+++ ModelDefs/architectures.py
@@ -440,7 +440,7 @@ class VGG(ModelArchitecture):
         if dropout:
             self.classifier = nn.Sequential(
                 nn.Dropout(),
-                nn.Linear(2048, 512),
+                nn.Linear(512, 512),
                 nn.ReLU(True),
                 nn.Dropout(),
                 nn.Linear(512, 512),
@@ -449,7 +449,7 @@ class VGG(ModelArchitecture):
             )
         else:
             self.classifier = nn.Sequential(
-                nn.Linear(2048, 512),
+                nn.Linear(512, 512),
                 nn.ReLU(True),
                 nn.Linear(512, 512),
                 nn.ReLU(True),
@@ -494,7 +494,7 @@ def make_layers(cfg, batch_norm=False):
 
 
 cfg = {
-    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512],
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
     'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M',
