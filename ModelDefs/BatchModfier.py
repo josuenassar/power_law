43ef34090ce9eebc39daa789f0167ee8c6c34b3b
Josue <josue.nassar@stonybrook.edu>
2020-10-19 18:17:18 -0400

    remove perturb

--- ModelDefs/BatchModfier.py
+++ ModelDefs/BatchModfier.py
@@ -86,13 +86,13 @@ class AdversarialTraining(BatchModifier):
         return x, ell.item()
 
     def FGSM(self, x_nat, y):
-        perturb = 2 * self.eps * torch.rand(x_nat.shape, device=x_nat.device) - self.eps
-        if x_nat.shape[1] == 1:
-            # if just one channel, then lb and ub are just numbers
-            x_nat = torch.clamp(x_nat + perturb, self.lb, self.ub)
-        else:
-            # for more than one channel, need channel specific lb and ub
-            x_nat = self.clip(x_nat + perturb, self.lb, self.ub)
+        # perturb = 2 * self.eps * torch.rand(x_nat.shape, device=x_nat.device) - self.eps
+        # if x_nat.shape[1] == 1:
+        #     # if just one channel, then lb and ub are just numbers
+        #     x_nat = torch.clamp(x_nat + perturb, self.lb, self.ub)
+        # else:
+        #     # for more than one channel, need channel specific lb and ub
+        #     x_nat = self.clip(x_nat + perturb, self.lb, self.ub)
         jacobian, ell = self.get_jacobian(x_nat, y)  # get jacobian
         x_nat = x_nat.detach()
         with torch.no_grad():
